a0221cd8c02df75511c3687f7a8ab23a494be657
RTChou67 <3023207450@tju.edu.cn>
2025-11-19 02:55:20 +0800

    dev

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,20 +1,36 @@
 return {
-	"stevearc/aerial.nvim",
-	opts = {
-		layout = {
-			default_direction = "right",
-			width = 30,
-		},
-		on_attach = function(_)
-			vim.cmd("AerialOpen")
-		end,
-	},
-	dependencies = {
-		"neovim/nvim-lspconfig",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	config = function(_, opts)
-		require("aerial").setup(opts)
-		vim.keymap.set("n", "<leader>o", "<cmd>AerialToggle<CR>", { desc = "Toggle Outline" })
-	end,
+    "stevearc/aerial.nvim",
+    opts = {
+        layout = {
+            default_direction = "right",
+            width = 30,
+        },
+        default_mode = "vsplit",
+        -- 添加这个配置来控制显示的详细程度
+        filter_kind = {
+            "Class",
+            "Constructor",
+            "Enum",
+            "Function",
+            "Interface",
+            "Module",
+            "Method",
+            "Struct",
+            "Variable",
+            "Constant",
+            "Field",
+            "Property",
+        },
+        on_attach = function(_)
+            vim.cmd("AerialOpen")
+        end,
+    },
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = function(_, opts)
+        require("aerial").setup(opts)
+        vim.keymap.set("n", "<leader>o", "<cmd>AerialToggle<CR>", { desc = "Toggle Outline" })
+    end,
 }
--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,67 +1,71 @@
 return {
-	"williamboman/mason-lspconfig.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		{ "folke/neodev.nvim" },
-		{ "williamboman/mason.nvim" },
-		{ "neovim/nvim-lspconfig" },
-		{ "hrsh7th/cmp-nvim-lsp" },
-	},
-	config = function()
-		-- 1. 加载依赖
-		require("neodev").setup()
-		local lspconfig = require("lspconfig") -- 现在加载是安全的
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-		local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-		end
+    "williamboman/mason-lspconfig.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        { "folke/neodev.nvim" },
+        { "williamboman/mason.nvim" },
+        { "neovim/nvim-lspconfig" },
+        { "hrsh7th/cmp-nvim-lsp" },
+    },
+    config = function()
+        -- 1. 加载依赖
+        require("neodev").setup()
+        local lspconfig = require("lspconfig") -- 现在加载是安全的
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+        local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
 
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"pyright",
-				"texlab",
-			},
-			automatic_installation = true,
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "pyright",
+                "texlab",
+            },
+            automatic_installation = true,
 
-			handlers = {
-
-				function(server_name)
-					lspconfig[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
-				["lua_ls"] = function()
-					require("lspconfig").lua_ls.setup({
-						capabilities = capabilities,
-
-						root_markers = {
-							".luarc.json",
-							".luarc.jsonc",
-							".luacheckrc",
-							".stylua.toml",
-							"stylua.toml",
-							"selene.toml",
-							"selene.yml",
-							".git",
-						},
-						settings = {
-							Lua = {
-								runtime = { version = "LuaJIT" },
-								diagnostics = { globals = { "vim", "require" } },
-								workspace = {
-									library = vim.api.nvim_get_runtime_file("", true),
-									checkThirdParty = false,
-								},
-								telemetry = { enable = false },
-							},
-						},
-					})
-				end,
-			},
-		})
-	end,
+            handlers = {
+                function(server_name)
+                    local lsp_util = require("lspconfig.util")
+                    lspconfig[server_name].setup({
+                        capabilities = capabilities,
+                        root_dir = lsp_util.root_pattern(
+                            ".git",
+                            "package.json",
+                            "Makefile"
+                        ) or vim.loop.cwd(),
+                    })
+                end,
+                ["lua_ls"] = function()
+                    require("lspconfig").lua_ls.setup({
+                        capabilities = capabilities,
+                        root_markers = {
+                            ".luarc.json",
+                            ".luarc.jsonc",
+                            ".luacheckrc",
+                            ".stylua.toml",
+                            "stylua.toml",
+                            "selene.toml",
+                            "selene.yml",
+                            ".git",
+                        },
+                        settings = {
+                            Lua = {
+                                runtime = { version = "LuaJIT" },
+                                diagnostics = { globals = { "vim", "require" } },
+                                workspace = {
+                                    library = vim.api.nvim_get_runtime_file("", true),
+                                    checkThirdParty = false,
+                                },
+                                telemetry = { enable = false },
+                            },
+                        },
+                    })
+                end,
+            },
+        })
+    end,
 }
