77d3dd4c29f7c4f13d5c0440daa2afd92e99070c
RTChou67 <3023207450@tju.edu.cn>
2026-03-23 11:47:36 +0800

    feat: organize which-key groups

--- lua/plugins/editor/which-key.lua
+++ lua/plugins/editor/which-key.lua
@@ -2,6 +2,24 @@ return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
 	config = function()
-		require("which-key").setup()
+		local wk = require("which-key")
+
+		wk.setup()
+		wk.add({
+			{ "<leader>b", group = "Buffers" },
+			{ "<leader>c", group = "Code" },
+			{ "<leader>e", group = "Explorer" },
+			{ "<leader>f", group = "Find" },
+			{ "<leader>g", group = "Git" },
+			{ "<leader>l", group = "LSP" },
+			{ "<leader>m", group = "Markdown" },
+			{ "<leader>n", group = "Search" },
+			{ "<leader>o", group = "Outline" },
+			{ "<leader>p", group = "Pick" },
+			{ "<leader>q", group = "Quit / Session" },
+			{ "<leader>r", group = "Rename / Refactor" },
+			{ "<leader>w", group = "Windows" },
+			{ "<leader>x", group = "Diagnostics" },
+		})
 	end,
 }
